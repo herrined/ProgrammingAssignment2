4a8a352b447bea1d86a0a1428ef62d344da40b09
Edward Herring <edherrin@icloud.com>
2020-09-29 08:20:16 -0500

    Completed homework
    ProgrammingAssignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix creates an invertable matrix
+## and sets up a persistent environment for
+## the variable values.
+## cacheSolve creates an inverted matrix.
+## Use:
+##  Create an invertable matrix:
+##    a <- matrix(c(1,2,3,4),2,2)
+##  Instantiate the function:
+##    a1 <- makeCacheMatrix(a)
+##  Inspect matrix a
+##    a
+##  Invert matrix a
+##    cacheSolve(a1)
+##  Run cacheSolve(a1) a second time
+##  to retrieve to get the cached value.
+##
+##
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inverse) m <<- inverse
+  getinv <- function() m
+  list(set = set,
+       get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
-
 
 ## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()
+  if (!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(m)
+  m
 }
+
+
